a7f30e2b2f063b16e2288859e563c9db27f28c8a
oedipux <mertkimyonsen@hotmail.com>
2019-08-21 22:14:26 +0300

    Category Model updated

--- dotnetcorerestfulapi/Models/Category.cs
+++ dotnetcorerestfulapi/Models/Category.cs
@@ -2,6 +2,8 @@ namespace dotnetcorerestfulapi.Models
 {
     public class Category
     {
-        
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public IList<Product> Products { get; set; } = new List<Product>();
     }
 }
